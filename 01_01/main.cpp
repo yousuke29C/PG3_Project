25136a71e31502d0d044f4920fb69d37a26774c8
LC1C_29 <k021g1384@g.neec.ac.jp>
2022-10-11 16:34:59 +0900

    01_02

--- 01_01/main.cpp
+++ 01_01/main.cpp
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
-template <typename T>
-
-T Min(T a, T b) {
-	if (b > a) {
-		return static_cast<T>(a);
+int Recursive(int n, int salary) {
+	if (n <= 1) {
+		return salary;
 	}
-	else {
-		return static_cast<T>(b);
-	}
-	return 0;
-} 
-
-template<>
-char Min<char>(char a, char b) {
-	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ\n");
-
-	return 0;
+	return (Recursive(n - 1, salary) * 2 - 50);
 }
 
 int main() {
-	printf("%d\n", Min<int>(1, 2));
-	printf("%f\n", Min<float>(1.0f, 2.0f));
-	printf("%lf\n", Min<double>(1.0, 2.0));
-	Min<char>('a', 'b');
+	
+	int n = 8;
+	int salary = 100;
+	int result;
 
-	return 0;
+	result = Recursive(n, salary);
+	printf("Ä‹N‰Â”\‚È’À‹à‘ÌŒn‚Å‚Ì%dŠÔ–Ú‚Ì‹‹ = %d\n", n, result);
+
+
+
+	return (0);
 }
