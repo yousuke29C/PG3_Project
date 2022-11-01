c3d0906de6443062d0c13a4e3b592fca33c51fd3
LC1C_29 <k021g1384@g.neec.ac.jp>
2022-11-01 14:18:09 +0900

    01_03

--- 01_01/main.cpp
+++ 01_01/main.cpp
@@ -1,22 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
 
-int Recursive(int n, int salary) {
-	if (n <= 1) {
-		return salary;
+typedef void(*PFunc)(int*);
+
+void DiceResult(int* n) {
+	srand(time(nullptr));
+	int dice = rand() % 5 + 1;
+
+	if (dice % 2 == 0) {
+		printf("アタリ\n");
 	}
-	return (Recursive(n - 1, salary) * 2 - 50);
+	else {
+		printf("ハズレ\n");
+	}
+
+	printf("サイコロの結果 → %d\n", dice);
 }
 
-int main() {
-	
-	int n = 8;
-	int salary = 100;
-	int result;
+void SetTimeout(PFunc p, int secound, int number) {
+	Sleep(secound);
+	p(&number);
+}
 
-	result = Recursive(n, salary);
-	printf("再起可能な賃金体系での%d時間目の時給 = %d\n", n, result);
+int main(void) {
+	PFunc p;
 
+	int number = 0;     // 番号
+	int secound = 3000; // 秒数
 
+	printf("半(奇数)だと思ったら [ 1 ] 、 丁(偶数)だと思ったら [ 0 ]を入力してください\n↓\n");
+	scanf_s("[ %d ]", &number);
 
-	return (0);
+	p = DiceResult;
+	SetTimeout(p, secound, number);
+
+	return 0;
 }
