59bc2e2193ea14b0c9697a5d3eb51d8fd67b970c
LC1C_29 <k021g1384@g.neec.ac.jp>
2022-11-29 13:51:41 +0900

    確認課題02_01

--- 01_01/main.cpp
+++ 01_01/main.cpp
@@ -1,16 +1,63 @@
 #include <stdlib.h>
-#include <vector>
+#include <list>//listコンテナ
+#include <iterator>
+#include <string>
+#include <iostream>
+
 using namespace std;
 
-int main() {
+int main()
+{
+#pragma region
+	
+#pragma endregion
 
-	// int型配列　a　(要素数：10)
-    //int a[10];
-	// int型の可変長配列 b (要素数：0)
-	vector<int> b;
+	list<const char*>Station = { "Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani",
+						"Nippori","Tabata","Komagome","Sugamo","Otsuka" ,
+						"Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo","Shinjuku",
+						"Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki",
+						"Shinagawa","Tamachi","Hamamatsutyo","Shinbashi",
+						"Yurakutyo" };
 
-	system("pause");
+	cout << "---1970年---\n";
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+
+	//西日暮里を追加
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		if (*itr == "Tabata")
+		{
+			itr = Station.insert(itr, "Nishinippori");
+			itr++;
+		}
+	}
+
+	cout << "---2019年---\n";
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+
+	//高輪ゲートウェイを追加
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		if (*itr == "Tamachi")
+		{
+			itr = Station.insert(itr, "Takanawa Gateway");
+			itr++;
+		}
+	}
+
+	cout << "---2022年---\n";
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+
 	return 0;
-
-
 }
+
+
