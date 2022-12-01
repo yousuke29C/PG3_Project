a0548fbe89227545403f61092cb054ceb9bcf775
LC1C_29 <k021g1384@g.neec.ac.jp>
2022-12-01 20:13:04 +0900

    02_02

--- 01_01/main.cpp
+++ 01_01/main.cpp
@@ -1,55 +1,74 @@
 ﻿#include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
+//単方向リストの構造体の定義
 typedef struct cell
 {
 	int val;
 	struct cell* next;
 }CELL;
 
-void create(CELL* list, int val);
-void index(CELL* list);
+void Create(CELL* currentCell, int val);
+void Index(CELL* head);
 
 int main()
 {
+
 	int val;
-	//�擪�̃Z����錾
+
+	//先頭のセルを宣言
 	CELL head;
 	head.next = nullptr;
 
 	while (true)
 	{
-		printf(" \n");
+		printf("数字を入力してください\n");
 
 		scanf_s("%d", &val);
 
-		create(&head, val);
+		//最後尾にリストを追加
+		Create(&head, val);
 
-		index(&head);
+		//リスト一覧の表示
+		Index(&head);
 	}
 	return 0;
 }
 
-void create(CELL* list, int val) {
-	CELL* NewCell;
-	NewCell = (CELL*)malloc(sizeof(CELL));
 
-	NewCell->val = val;
-	NewCell->next = nullptr;
+//セルを新規作成する関数
+void Create(CELL* currentCell, int val)
+{
+	CELL* newCell;
 
-	while (list->next != nullptr) {
-		list = list->next;
+	//新規作成するセル分のメモリを確保する
+	newCell = (CELL*)malloc(sizeof(CELL));
+
+	if (newCell)
+	{
+		newCell->val = val;
+		newCell->next = nullptr;
 	}
-	list->next = NewCell;
+
+	//最後(最新)のセルのアドレスの1つ目の処理は引数から持ってきた
+	//リストのうち最初のセルのアドレスが該当する
+	while (currentCell->next != nullptr)
+	{
+		currentCell = currentCell->next;
+	}
+
+	currentCell->next = newCell;
 }
 
-void index(CELL* list)
+void Index(CELL* head)
 {
-	while (list->next != nullptr)
+	printf("{");
+	while (head->next != nullptr)
 	{
-		list = list->next;
-		printf("[%d]\n", list->val);
+		head = head->next;
+		printf("%d,", head->val);
 	}
+	printf("}\n\n");
 }
 
 
